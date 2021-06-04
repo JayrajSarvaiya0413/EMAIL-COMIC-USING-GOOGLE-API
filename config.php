2543167e0c7a3b93eedbe7ba8ae8715ffe88bd63
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-04 11:29:20 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $google_client = new Google_Client();
 $google_client->setClientId('539388184524-hpno27um91fqb44vob0a8l6b9fv89c87.apps.googleusercontent.com');
 $google_client->setClientSecret('DeU2ib3COsW4oK8eesyAg4UK');
 
-$google_client->setRedirectUri('index.php');
+$google_client->setRedirectUri('http://localhost/rtcamp_jayraj_email_challenge/index.php');
 
 $google_client->addScope('email');
 $google_client->addScope('profile');
