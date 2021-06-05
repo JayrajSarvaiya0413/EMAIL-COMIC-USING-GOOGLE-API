484511eff20b62eb2d0e678e40da13623dd598cc
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-05 08:31:53 +0530

    Update RemortSQL

--- emailregister.php
+++ emailregister.php
@@ -1,8 +1,8 @@
  <?php
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "rtcampemail";
+ $servername = 'bf1adw57uf1skqz6m6dk-mysql.services.clever-cloud.com:3306';
+ $username = 'uvfmqmibbki1zaip';
+ $password = 'p9JlNuAesTQtixenuEdR';
+ $dbname = 'bf1adw57uf1skqz6m6dk';
 
 // Create connection
  $conn = new mysqli($servername, $username, $password, $dbname);
