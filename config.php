84b04eea70bad3b02fd3afb54c81d880c38cb674
JayrajSarvaiya0413 <jayrajsarvaiya0413@gmail.com>
2021-06-07 16:46:05 +0530

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $google_client = new Google_Client();
 $google_client->setClientId('539388184524-hpno27um91fqb44vob0a8l6b9fv89c87.apps.googleusercontent.com');
 $google_client->setClientSecret('DeU2ib3COsW4oK8eesyAg4UK');
 
-$google_client->setRedirectUri('https://emailcomicjayraj.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://emailcomic.cleverapps.io/index.php');
 
 $google_client->addScope('email');
 $google_client->addScope('profile');
